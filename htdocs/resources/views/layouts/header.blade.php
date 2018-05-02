3adaa746606b81865e918dc5771030add2154327
richardbishopme <rick@richardbishop.me>
2018-05-02 16:30:56 +0100

    Init

---
+++ htdocs/resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<header role="banner">
+    <div class="bg-black">
+        <div class="container flex">
+            <div class="flex-1">
+                <a href="https://www.gov.uk" class="logo" title="Go to the GOV.UK homepage" id="logo">
+                    <img src="/images/gov_uk_logotype_crown.png"> GOV.UK
+                </a>
+            </div>
+            <div class="flex-1">
+                <a class="text-white" href="#proposition-links">Menu</a>
+                <nav class="flex flex-col p-2">
+                    <a href="/home" class="text-white">NMDS-SC</a>
+                    <ul id="proposition-links" class="inline-flex list-reset">
+                        <li><a class="text-white" href="/records">Records</a></li>
+                        <li><a class="text-white" href="/reports">Reports &amp; data</a></li>
+                        <li><a class="text-white" href="/account/settings">Account</a></li>
+                        <li><a class="text-white" href="#">About</a></li>
+                        <li><a class="text-white" href="/help">Help</a></li>
+                        <li><a class="text-white" href="/login">Logout</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="container border-b-8 border-blue" />
+</header>
