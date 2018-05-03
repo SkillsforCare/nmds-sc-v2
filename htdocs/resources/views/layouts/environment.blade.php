d44d78f3d745c833b946f00d4e6a5cf072e7d32d
richardbishopme <rick@richardbishop.me>
2018-05-03 14:47:46 +0100

    Fix alpha component template

--- htdocs/resources/views/layouts/environment.blade.php
+++ htdocs/resources/views/layouts/environment.blade.php
@@ -8,25 +8,20 @@
 @endif
 
 @if(config('app.env') == 'development')
-    @component('layouts.phase-banner')
-        @slot('title')
-            ALPHA - Development
-        @endslot
-        <span>This is the development site for ALPHA.</span>
-    @endcomponent
-<div class="phase-banner">
-    <p>
-        <strong class="phase-tag">ALPHA - Development</strong>
-        <span>This is a new service – your <a href="#">feedback</a> will help us to improve it.</span>
-    </p>
-</div>
+@component('layouts.phase-banner')
+    @slot('title')
+        ALPHA - Development
+    @endslot
+    <span>This is the development site for ALPHA.</span>
+@endcomponent
 @endif
 
 @if(config('app.env') == 'alpha')
-<div class="phase-banner">
-    <p>
-        <strong class="phase-tag">ALPHA</strong>
-        <span>This is a new service – your <a href="#">feedback</a> will help us to improve it.</span>
-    </p>
-</div>
+@component('layouts.phase-banner')
+    @slot('title')
+        ALPHA
+    @endslot
+    <span>This is a new service – your <a href="#">feedback</a> will help us to improve it.</span>
+@endcomponent
+
 @endif
