bfe16675240e45073cbfe3ec211d1715708dd047
richardbishopme <rick@richardbishop.me>
2018-05-15 16:18:44 +0100

    Build assets

--- htdocs/public/js/app.js
+++ htdocs/public/js/app.js
@@ -47381,7 +47381,7 @@ if (false) {
 /* 43 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\n  padding-bottom: 3em;Q\r\n                     ^\r\n      Property \"Q\" must be followed by a ':'\r\n      in C:\\laragon\\www\\nmds-sc-v21\\htdocs\\resources\\assets\\sass\\app.scss (line 11, column 23)\n    at runLoaders (C:\\laragon\\www\\nmds-sc-v21\\htdocs\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at C:\\laragon\\www\\nmds-sc-v21\\htdocs\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\laragon\\www\\nmds-sc-v21\\htdocs\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (C:\\laragon\\www\\nmds-sc-v21\\htdocs\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (C:\\laragon\\www\\nmds-sc-v21\\htdocs\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (C:\\laragon\\www\\nmds-sc-v21\\htdocs\\node_modules\\neo-async\\async.js:7974:18)\n    at options.error (C:\\laragon\\www\\nmds-sc-v21\\htdocs\\node_modules\\node-sass\\lib\\index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
