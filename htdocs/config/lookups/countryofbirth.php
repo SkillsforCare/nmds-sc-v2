da3f90036758a5cd7d433a098f3d1cad877b77d2
richardbishopme <rick@richardbishop.me>
2018-05-29 17:05:32 +0100

    Fix lookups

--- htdocs/config/lookups/countryofbirth.php
+++ htdocs/config/lookups/countryofbirth.php
@@ -2,4 +2,4 @@
 
 return [[ 'text' => 'United Kingdom', 'value' => 826],
     [ 'text' => 'Not United Kingdom', 'value' => 998],
-    [ 'text' => 'Don\'t know, 'value' => 999]];
+    [ 'text' => 'Don\'t know', 'value' => 999]];
