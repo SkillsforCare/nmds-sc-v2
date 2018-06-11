4a9b2d511ad138fcc923ccf8c3955665724b2ece
richardbishopme <rick@richardbishop.me>
2018-06-11 15:02:52 +0100

    Add tests

--- htdocs/routes/web.php
+++ htdocs/routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return redirect()->route('login');
 })->name('pages.home');
 
+Route::get('/users', function() {
+    $users = App\User::with('person', 'roles')->orderBy('username')->get();
+    return view('pages.users', compact('users'));
+});
+
 
 Route::get('/landing', function() {
 
@@ -15,7 +20,7 @@ Route::get('/landing', function() {
     if(auth()->user()->hasRole('edit-user'))
         return redirect()->route('records.index');
 
-})->middleware('auth')->name('pages.landing');;
+})->middleware('auth')->name('pages.landing');
 
 // Analyst reports.
 Route::name('reports.')->prefix('reports')->namespace('AnalystUser')->group(function () {
@@ -32,16 +37,16 @@ Route::name('records.')->prefix('records')->group(function () {
     Route::middleware(['auth', 'role:edit-user'])->group(function () {
         Route::get('/', 'RecordIndexController')->name('index');
         Route::get('/workers', 'WorkerController@index')->name('workers');
-        Route::post('/workers', 'WorkerController@store')->name('workers.store');
-        Route::get('/workers/create', 'WorkerController@create')->name('workers.create');
-        Route::get('/workers/{worker}/edit/{section}', 'WorkerController@edit')->name('workers.edit');
-        Route::get('/workers/{worker}', 'WorkerController@show')->name('workers.show');
+        Route::post('/workers', 'WorkerController@store')->name('workers.store')->middleware(['can:create,App\Worker']);
+        Route::get('/workers/create', 'WorkerController@create')->name('workers.create')->middleware(['can:create,App\Worker']);
+        Route::get('/workers/{worker}/edit/{section}', 'WorkerController@edit')->name('workers.edit')->middleware(['can:view,worker']);
+        Route::get('/workers/{worker}', 'WorkerController@show')->name('workers.show')->middleware(['can:view,worker']);;
         Route::get('/establishment/{establishment?}', 'EstablishmentController@show')->name('establishment.show');
     });
 });
 
-Route::middleware('auth')->put('question_answers_bulk/{worker}/update', 'QuestionAnswerBulkController@update')->name('question_answer_bulk_update');
-Route::middleware('auth')->put('finish_worker_record/{worker}/update', 'FinishWorkerRecordController')->name('finish_worker_record');
+Route::middleware(['auth', 'can:update,worker'])->put('question_answers_bulk/{worker}/update', 'QuestionAnswerBulkController@update')->name('question_answer_bulk_update');
+Route::middleware(['auth', 'can:update,worker'])->put('finish_worker_record/{worker}/update', 'FinishWorkerRecordController')->name('finish_worker_record');
 
 Route::get('/about', function () {
     return view('pages.about');
